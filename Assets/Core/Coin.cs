9ef35bc786ffeca900323dfec74f94ec4af46ff7
Alexandra Averyanova <alexaver11801@gmail.com>
2023-06-20 23:04:31 +0300

    initial

---
+++ Assets/Core/Coin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Coin
+{
+    public class Coin : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private MeshRenderer renderer;
+
+        private void Update()
+        {
+            var targetRotation = Quaternion.Euler(transform.eulerAngles + Vector3.up * rotationSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime);
+        }
+
+        public void SetRandomColor()
+        {
+            var randomVector = Random.onUnitSphere;
+            renderer.material.color = new Color(randomVector.x, randomVector.y, randomVector.z);
+        }
+    }
+}
---
+++ Assets/Core/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+namespace Coin
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private Camera camera;
+        [SerializeField] private float raycastDistance;
+        private InputActions inputActions;
+
+        private void Awake()
+        {
+            inputActions = new InputActions();
+            EnhancedTouchSupport.Enable();
+            inputActions.Controls.Pointer.performed += _ => ProcessTap();
+        }
+
+        private void OnEnable()
+        {
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        private void ProcessTap()
+        {
+            var pointerPosition = inputActions.Controls.PointerPosition.ReadValue<Vector2>();
+            if (Physics.Raycast(camera.ScreenPointToRay(pointerPosition), out var hit, raycastDistance) &&
+                hit.transform.TryGetComponent<Coin>(out var coin))
+            {
+                coin.SetRandomColor();
+            }
+        }
+    }
+}
